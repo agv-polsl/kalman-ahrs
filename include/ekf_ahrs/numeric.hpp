7f85e66a8f15d4416cfecdeae16fcdd07721ab18
Maciej Ziaja <maciejzjg@gmail.com>
2020-03-30 14:37:45 +0200

    Implement basic array arithmetic and printing
    Basic means multiplication and transpose.

--- include/ekf_ahrs/numeric.hpp
+++ include/ekf_ahrs/numeric.hpp
@@ -1,4 +1,5 @@
 #include <array>
+#include <ostream>
 
 namespace ekfn { /* Extended kalman filter numerics */
 
@@ -8,18 +9,38 @@ using array_2d = std::array<std::array<T, M>, N>;
 template <typename T, size_t Nl, size_t Ml, size_t Nr, size_t Mr>
 array_2d<T, Nl, Mr> operator*(const array_2d<T, Nl, Ml>& lhs,
                               const array_2d<T, Nr, Mr>& rhs) {
+    static_assert(Ml == Nr, "Can not multiply matrices of given size");
     array_2d<T, Nl, Mr> ret = {0};
+
+    for (size_t i = 0; i < Nl; i++) {
+        for (size_t j = 0; j < Ml; j++) {
+            for (size_t k = 0; k < Mr; k++) {
+                ret[i][j] += lhs[i][k] * rhs[k][j];
+            }
+        }
+    }
     return ret;
 }
 
 template <typename T, size_t N, size_t M>
-std::ostream& operator<<(std::ostream& stream, const array_2d<T, N, M>& arr) {
-    return stream;
+std::ostream& operator<<(std::ostream& os, const array_2d<T, N, M> arr) {
+    for (auto row : arr) {
+        for (auto el : row) {
+            os << el << ' ';
+        }
+        os << '\n';
+    }
+    return os;
 }
 
 template <typename T, size_t N, size_t M>
 array_2d<T, N, M> transpose(const array_2d<T, N, M>& arr) {
     array_2d<T, N, M> ret = arr;
+    for (size_t i = 0; i < N; i++) {
+        for (size_t j = 0; j < M; j++) {
+            ret[i][j] = arr[j][i];
+        }
+    }
     return ret;
 }
 
