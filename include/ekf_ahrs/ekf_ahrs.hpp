b40c46ef8aab600ffbf01e7cb123e5265a8585fb
Maciej Ziaja <maciejzjg@gmail.com>
2020-04-01 19:11:23 +0200

    Update formatting

--- include/ekf_ahrs/ekf_ahrs.hpp
+++ include/ekf_ahrs/ekf_ahrs.hpp
@@ -1,5 +1,6 @@
-#include "ekf_ahrs/numeric.hpp"
 #include <cmath>
+
+#include "ekf_ahrs/numeric.hpp"
 
 namespace ekfn {
 
@@ -10,4 +11,4 @@ double calc_pitch(const double acc_x, double const acc_z) {
 double calc_roll(const double acc_y, const double acc_z) {
     return std::atan(acc_y / ((acc_y * acc_y) + (acc_z * acc_z)));
 }
-}
+}  // namespace ekfn
