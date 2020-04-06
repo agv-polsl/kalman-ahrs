1e867458b4660da3e9ed94e8d8799a184895c4ff
Maciej Ziaja <maciejzjg@gmail.com>
2020-04-06 21:22:15 +0200

    Add sensor mock

--- test/mocks/mock.hpp
+++ test/mocks/mock.hpp
@@ -1,2 +1,8 @@
-#include "calculator/square_root.hpp"
 #include <gmock/gmock.h>
+
+#include "ahrs/ahrs_sensors.hpp"
+
+class SensorMock : Sensor {
+   public:
+    MOCK_METHOD(sensor_readout, read, void);
+};
